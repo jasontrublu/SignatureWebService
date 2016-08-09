3dfec45b2cf836d59e59ce9018a94003ebedcfeb
Jason Peper <trublu@c-peper.de>
2016-08-09 23:20:34 +0100

    add test coverage for health check

--- src/test/java/de/trublu/signature/SignatureResourceSpec.groovy
+++ src/test/java/de/trublu/signature/SignatureResourceSpec.groovy
@@ -5,6 +5,7 @@ import de.trublu.signature.application.SignatureConfiguration
 import groovyx.net.http.ContentType
 import groovyx.net.http.HttpResponseDecorator
 import groovyx.net.http.RESTClient
+import io.dropwizard.testing.ConfigOverride
 import io.dropwizard.testing.DropwizardTestSupport
 import io.dropwizard.testing.ResourceHelpers
 import spock.lang.Specification
@@ -13,7 +14,9 @@ class SignatureResourceSpec extends Specification {
 
     public static final DropwizardTestSupport<SignatureConfiguration> SUPPORT =
             new DropwizardTestSupport<SignatureConfiguration>(SignatureApplication.class,
-                    ResourceHelpers.resourceFilePath("signature.yaml")
+                    ResourceHelpers.resourceFilePath("signature.yaml"),
+                    ConfigOverride.config("server.applicationConnectors[0].port", "0"),
+                    ConfigOverride.config("server.adminConnectors[0].port", "0")
             );
 
     def setup() {
@@ -22,12 +25,22 @@ class SignatureResourceSpec extends Specification {
 
     def "get static signature"() {
         setup:
-            def client = new RESTClient("http://localhost:" + "8080")// RULE.getLocalPort())
+            def client = new RESTClient("http://localhost:" +  SUPPORT.getLocalPort())
         when:
             def response = client.get(path: "/signature") as HttpResponseDecorator
         then:
             response.status == 200
             response.contentType == ContentType.JSON.toString()
             response.data.text == "-- \na .signature"
+    }
+
+    def "get ADMIN/healtcheck"() {
+        setup:
+            def client = new RESTClient("http://localhost:" +  SUPPORT.getAdminPort())
+        when:
+            def response = client.get(path: "/healthcheck") as HttpResponseDecorator
+        then:
+            response.status == 200
+            response.contentType == ContentType.JSON.toString()
     }
 }
